369951cb10b9a01fdd78d8b58a6bf2d3125a5610
Octavio Ignacio Rivera Perez <octavio.rivera02@inacapmail.cl>
2026-05-29 13:14:17 -0400

    update

--- suppages/form.js
+++ suppages/form.js
@@ -1,9 +1,25 @@
+let fileHandle;
+
 function EmailValido(email) {
     const patronEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return patronEmail.test(email);
 }
 
-document.getElementById('Registro').addEventListener('submit', function(event) {
+function descargarCSV(datos) {
+    const csvContent = "data:text/csv;charset=utf-8," 
+        + "Nombre,Edad,Email\n"
+        + `${datos.nombre},${datos.edad},${datos.email}`;
+
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "registro_usuarios.csv");
+    document.body.appendChild(link);
+    link.click(); 
+    document.body.removeChild(link);
+}
+
+document.getElementById('Registro').addEventListener('submit', async function(event) {
 
     let errores = [];
 
@@ -48,8 +64,25 @@ document.getElementById('Registro').addEventListener('submit', function(event) {
     }
 
     if (errores.length > 0) {
-        event.preventDefault();
         alert(errores.join('\n'));
-    }
+    } else {
+        const nombre = document.getElementById('nombre').value;
+        const edad = document.getElementById('edad').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const nuevaLinea = `${nombre},${edad},${email},${password}\n`;
 
+        const blob = new Blob([nuevaLinea], { type: 'text/csv' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        
+        a.href = url;
+        a.download = "form.csv";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        alert("Formulario enviado");
+        this.reset();
+    }
 });
